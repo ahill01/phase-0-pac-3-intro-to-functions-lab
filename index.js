e950161bf3d44a08e7a776bd17a32d84d4f8ea6d
Allison <amh03160@gmail.com>
2022-02-07 17:27:18 -0600

    added switch statements instead of if else

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ function logWhisper(string){
 };
 
 function sayHiToHeadphonedRoommate(string){
-    if(string === string.toLowerCase()){
+  switch(true) {
+      case string === string.toLowerCase(): 
         return "I can't hear you!";
-    } else if (string === string.toUpperCase()) {
+    case string === string.toUpperCase(): 
         return "YES INDEED!";
-    } else if(string ==="Let's have dinner together!") {
+    case string ==="Let's have dinner together!": 
         return "I would love to!"
     }
 };
